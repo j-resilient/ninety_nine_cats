d382919d9ae497dc1f45fbe1d1fe44824c7ae20e
J Resilient <j.resilient.1@gmail.com>
2020-03-04 10:06:48 -0500

    Add new view, and the new and create actions

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -8,4 +8,23 @@ class CatsController < ApplicationController
         @current_cat = Cat.find_by(id: params[:id])
         render :show
     end
+
+    def new
+        # @cat = Cat.new
+        render :new
+    end
+
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render :new
+        end
+    end
+
+    private
+    def cat_params
+        params.require(:cat).permit(:name, :sex, :color, :birth_date, :description)
+    end
 end
