d912fae6c45523896c5f3558e0a8108767400828
J Resilient <j.resilient.1@gmail.com>
2020-03-04 12:41:22 -0500

    Add does_not_overlap_approved_request validation

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -1,12 +1,14 @@
 class CatRentalRequest < ApplicationRecord
     validates :cat_id, :start_date, :end_date, presence: true
     validates :status, inclusion: { in: %w(PENDING APPROVED DENIED) }
+    validates :does_not_overlap_approved_request
 
     belongs_to :cat,
         primary_key: :id,
         foreign_key: :cat_id,
         class_name: :Cat
 
+    private
     def overlapping_requests
         CatRentalRequest
             .where(cat_id: self.cat_id)
@@ -16,5 +18,9 @@ class CatRentalRequest < ApplicationRecord
 
     def overlapping_approved_requests
         overlapping_requests.where(status: "APPROVED")
+    end
+
+    def does_not_overlap_approved_request
+        overlapping_approved_requests.exists?
     end
 end
