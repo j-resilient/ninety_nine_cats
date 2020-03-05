7ebc3f2e6f817f67a1d2d9cfd520980c7ff61090
J Resilient <j.resilient.1@gmail.com>
2020-03-05 14:10:49 -0500

    Add code so that approve/deny buttons show up on cat show views

--- app/controllers/cat_rental_requests_controller.rb
+++ app/controllers/cat_rental_requests_controller.rb
@@ -14,6 +14,17 @@ class CatRentalRequestsController < ApplicationController
         end
     end
 
+    def approve_or_deny
+        request = CatRentalRequest.find_by(id: params[:id])
+        # if params[:option] == "approve"
+        #     request.approve!
+        # else
+        #     request.deny!
+        # end
+        params[:option] == "approve" ? request.approve! : request.deny!
+        redirect_to cat_url(id: request.cat_id)       
+    end
+
     private
     def request_params
         params.require(:request).permit(:cat_id, :start_date, :end_date, :status)
