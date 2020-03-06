4b647f25b7f3582f771b8b89e6ef4b43fbc23668
J Resilient <j.resilient.1@gmail.com>
2020-03-06 10:19:45 -0500

    Check that self isn't already denied in #does_not_overlap_approved_request

--- app/models/cat_rental_request.rb
+++ app/models/cat_rental_request.rb
@@ -41,6 +41,7 @@ class CatRentalRequest < ApplicationRecord
     end
 
     def does_not_overlap_approved_request
+        return if self.status == 'DENIED'
         if overlapping_approved_requests.exists?
             errors[:overlapping] << "request already approved"
         end
