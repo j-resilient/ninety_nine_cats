d6437310b8982626fcd5e8037b9e8b4d63bfc0af
J Resilient <j.resilient.1@gmail.com>
2020-03-06 10:03:09 -0500

    Remove extraneous routes, redirect '/' to '/cats'

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cats
-  resources :cat_rental_requests, only: [:new, :create]
-  resources :cat_rental_requests do
+    resources :cat_rental_requests, only: [:new, :create] do
     member do
       patch 'approve_or_deny'
     end
   end
+
+  root to: redirect('/cats')
 end
